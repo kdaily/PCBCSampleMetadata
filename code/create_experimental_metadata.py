0ea510425e4474ba83ae596e5d51a3da8fb9dbca
Kenneth Daily <kenneth.daily@sagebase.org>
2014-11-12 14:27:26 -0800

    updated to use BootstrapExperimentalMetadata, added mRNA fastq

--- code/create_experimental_metadata.py
+++ code/create_experimental_metadata.py
@@ -1,12 +1,15 @@
-import re
+"""Use existing annotations and static files to make a table of experimental metadata.
+
+Works specifically for PCBC data - mRNA, miRNA, and methylation.
+
+"""
+
 
 import pandas as pd
 import synapseclient
-import ExperimentalMetadata
+import BootstrapExperimentalMetadata
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
-
 
 def main():
     import argparse
@@ -27,32 +30,48 @@ def main():
     args = parser.parse_args()
 
     syn = synapseclient.login(silent=True)
+    
+    ## mRNA metadata from bam, bed, fastq, and fpkm files
+    pcbcmeta = BootstrapExperimentalMetadata.MRNABamMetadata(syn)
+    pcbc_mrna_bam = pcbcmeta()
 
-    # pcbcmeta = ExperimentalMetadata.AllExperimentalMetadata(syn)
-    # pcbc_sample_metadata = pcbcmeta()
+    sys.stderr.write("Getting mRNA fastq data\n")
+    pcbcmeta = BootstrapExperimentalMetadata.MRNAFastqMetadata(syn)
+    pcbc_mrna_fastq = pcbcmeta()
 
-    pcbcmeta = ExperimentalMetadata.MRNAMetadata(syn)
-    pcbc_mrna = pcbcmeta()
-
-    pcbcmeta = ExperimentalMetadata.MIRNAMetadata(syn)
-    pcbc_mirna = pcbcmeta()
-
-    pcbcmeta = ExperimentalMetadata.MethylMetadata(syn)
+    pcbcmeta = BootstrapExperimentalMetadata.MRNABedMetadata(syn)
+    pcbc_mrna_bed = pcbcmeta()
+    
+    pcbcmeta = BootstrapExperimentalMetadata.MRNAFpkmMetadata(syn)
+    pcbc_mrna_fpkm = pcbcmeta()
+    
+    ## miRNA metadata from Fastq and Expr files
+    pcbcmeta = BootstrapExperimentalMetadata.MIRNAFastqMetadata(syn)
+    pcbc_mirna_fastq = pcbcmeta()
+    
+    pcbcmeta = BootstrapExperimentalMetadata.MIRNAExprMetadata(syn)
+    pcbc_mirna_expr = pcbcmeta()
+    
+    pcbcmeta = BootstrapExperimentalMetadata.MethylMetadata(syn)
     pcbc_methyl = pcbcmeta()
 
-    pcbc_sample_metadata = pd.concat([pcbc_mrna, pcbc_mirna, pcbc_methyl],
+    pcbc_sample_metadata = pd.concat([pcbc_mrna_bam, pcbc_mrna_bed,
+                                      pcbc_mrna_fpkm,
+                                      pcbc_mrna_fastq,
+                                      pcbc_mirna_fastq, pcbc_mirna_expr,
+                                      pcbc_methyl],
                                      ignore_index=True)
 
     _keepcols = ["name", "id", "UID", "C4_Cell_Line_ID",
-                 "Diffname short", "dataType", "fileType"]
+                 "Diffname_short", "dataType", "fileType"]
 
     pcbc_sample_metadata = pcbc_sample_metadata[_keepcols]
 
     pcbc_sample_metadata.to_csv(args.outfile, sep="\t", index=False)
     
     if args.storetable:
-        tbl = ExperimentalMetadata.to_table(syn, pcbc_sample_metadata,
-                                            args.projectId)
+        tbl = BootstrapExperimentalMetadata.to_table(syn, pcbc_sample_metadata,
+                                                     args.projectId)
 
     # pcbcmeta.to_csv(args.outfile)
         
