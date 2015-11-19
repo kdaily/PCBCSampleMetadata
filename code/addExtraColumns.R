b7784566de2b035c0b84144c4c7187ffa0e8b79d
kdaily <kdaily@users.noreply.github.com>
2015-11-19 13:59:10 -0800

    added code

--- code/addExtraColumns.R
+++ code/addExtraColumns.R
@@ -1,7 +1,12 @@
 # Add extra columns to the metadata table
 
+# XIST data
+xistObj <- synGet("syn4313132")
+xistData <- read.delim(getFileLocation(xistObj), sep="\t")
+
 extraColumns <- function(x) {
-  x %>% 
+  x %>%
+    dplyr::left_join(xistData, by="C4_Cell_Line_ID") %>%
     dplyr::mutate(Cell_Type_of_Origin_Level2=ifelse(Cell_Type_of_Origin %in% c("CD34+ cells", "mononuclear"),
                                                     "blood",
                                                     as.character(Cell_Type_of_Origin)),
